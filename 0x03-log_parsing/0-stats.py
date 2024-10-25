c4efbe92d4ba77d47fe0a17814a6ce18c87bd64a
Keanen09 <bkeanen@gmail.com>
2024-10-25 11:28:19 +0000

    try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,75 @@
 #!/usr/bin/python3
-
+"""
+module: script that reads stdin line by line and computes metrics
+"""
 import sys
+import re
+import signal
+import os
+from typing import Any
 
 
-def print_msg(dict_sc, total_file_size):
-    """
-    Method to print
-    Args:
-        dict_sc: dict of status codes
-        total_file_size: total of the file
-    Returns:
-        Nothing
-    """
-
-    print("File size: {}".format(total_file_size))
-    for key, val in sorted(dict_sc.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
+sig_num = signal.Signals.SIGINT
+file_size = []
+status_codes = []
+possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
 
-total_file_size = 0
-code = 0
-counter = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+def control_loop() -> Any:
+    """The function that loops and finds a match from stdin"""
+    loop_count = 0
 
-try:
     for line in sys.stdin:
-        parsed_line = line.split()  # ✄ trimming
-        parsed_line = parsed_line[::-1]  # inverting
+        if loop_count == 10:
+            os.kill(os.getpid(), signal.SIGINT)
+            return
+        pattern = r'\w+'
+        matched_group = re.findall(pattern, line)
 
-        if len(parsed_line) > 2:
-            counter += 1
+        loop_count += 1
+        last_status_code = matched_group[-2]
+        last_file_size = matched_group[-1]
+        file_size.append(int(last_file_size))
+        status_codes.append(last_status_code)
 
-            if counter <= 10:
-                total_file_size += int(parsed_line[0])  # file size
-                code = parsed_line[1]  # status code
 
-                if (code in dict_sc.keys()):
-                    dict_sc[code] += 1
+def signal_handler(pid, signal) -> Any:
+    """a function to handle the sigint signal from the user
+    """
+    printer()
+    file_size.clear()
+    status_codes.clear()
 
-            if (counter == 10):
-                print_msg(dict_sc, total_file_size)
-                counter = 0
 
-finally:
-    print_msg(dict_sc, total_file_size)
+def int_values(list_strings):
+    """a function to make a list of integers
+    """
+    list_ints = []
+    for string in list_strings:
+        list_ints.append(int(string))
+
+    return list_ints
+
+
+def printer():
+    """A function that computes metrics
+    """
+    template = f'File size: {sum(file_size)}'
+    print(template)
+    visited = []
+    current_values = sorted(int_values(status_codes))
+    for code in current_values:
+        if code in visited:
+            continue
+        else:
+            visited.append(code)
+            count = status_codes.count(code)
+            loop_template = f'{code}: {count}'
+            print(loop_template)
+
+
+signal.signal(sig_num, signal_handler)
+
+if __name__ == '__main__':
+    while True:
+        control_loop()

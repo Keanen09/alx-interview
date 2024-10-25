1cdef047fc5dbbd4d455cb44d64238f243df767b
Keanen09 <bkeanen@gmail.com>
2024-10-25 11:33:52 +0000

    try

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,75 +1,48 @@
 #!/usr/bin/python3
 """
-module: script that reads stdin line by line and computes metrics
+a script that reads stdin line by line and computes metrics:
+Input format: <IP Address> - [<date>]
+"GET /projects/260 HTTP/1.1" <status code> <file size>
 """
-import sys
-import re
-import signal
-import os
-from typing import Any
+from sys import stdin
+
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
+}
+
+file = 0
 
 
-sig_num = signal.Signals.SIGINT
-file_size = []
-status_codes = []
-possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+def print_log():
+    """Prints the logs"""
+    print("File size: {}".format(file))
+    for stat in sorted(status_codes.keys()):
+        if status_codes[stat]:
+            print("{}: {}".format(stat, status_codes[stat]))
 
 
-def control_loop() -> Any:
-    """The function that loops and finds a match from stdin"""
-    loop_count = 0
-
-    for line in sys.stdin:
-        if loop_count == 10:
-            os.kill(os.getpid(), signal.SIGINT)
-            return
-        pattern = r'\w+'
-        matched_group = re.findall(pattern, line)
-
-        loop_count += 1
-        last_status_code = matched_group[-2]
-        last_file_size = matched_group[-1]
-        file_size.append(int(last_file_size))
-        status_codes.append(last_status_code)
-
-
-def signal_handler(pid, signal) -> Any:
-    """a function to handle the sigint signal from the user
-    """
-    printer()
-    file_size.clear()
-    status_codes.clear()
-
-
-def int_values(list_strings):
-    """a function to make a list of integers
-    """
-    list_ints = []
-    for string in list_strings:
-        list_ints.append(int(string))
-
-    return list_ints
-
-
-def printer():
-    """A function that computes metrics
-    """
-    template = f'File size: {sum(file_size)}'
-    print(template)
-    visited = []
-    current_values = sorted(int_values(status_codes))
-    for code in current_values:
-        if code in visited:
-            continue
-        else:
-            visited.append(code)
-            count = status_codes.count(code)
-            loop_template = f'{code}: {count}'
-            print(loop_template)
-
-
-signal.signal(sig_num, signal_handler)
-
-if __name__ == '__main__':
-    while True:
-        control_loop()
+if __name__ == "__main__":
+    count = 1
+    try:
+        for line in stdin:
+            try:
+                log = line.split()
+                if log[-2] in status_codes:
+                    status_codes[log[-2]] += 1
+                file += int(log[-1])
+            except Exception:
+                pass
+            if count % 10 == 0:
+                print_log()
+            count += 1
+    except KeyboardInterrupt:
+        print_log()
+        raise
+    print_log()
